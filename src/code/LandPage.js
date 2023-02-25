c7ad25cd28eb244795b3ca33160c3901aac26504
opvishu2 <gurfire2@gmail.com>
2023-02-25 23:55:58 +0530

    react-transition-group

--- src/code/LandPage.js
+++ src/code/LandPage.js
@@ -1,18 +1,58 @@
+
 import React, { useState } from 'react'
 import './LandPage.css';
 import self from '../assets/images/self.png'
 import Nav from './Nav';
+import { CSSTransition } from 'react-transition-group';
 
 
 export default function LandPage() {
-    const [nav_view, toogleNavView] = useState(false)
-    return (
-        <div className={nav_view == "left" ? "App" : "App_top"}>
-            <Nav nav_view={nav_view} toogleNavView={toogleNavView} />
-            <div className='content'>
-                <div className='left_c'><h1 className='typed'>Hi, Vishal M. here!</h1></div>
-                <div className='right_c'><img className="App-logo" alt="logo" src={self}></img></div>
+    const [top_nav, toggleTopNav] = useState(false)
+    const [side_bar, toggleSideBar] = useState(false)
+    return (<>
+        {!top_nav && !side_bar &&
+            <div className={"App"}>
+                <Nav top_nav={top_nav} toggleTopNav={toggleTopNav} side_bar={side_bar} toggleSideBar={toggleSideBar} />
+                <div className='content'>
+                    <div className='left_c'><h1 className='typed'>Hi, Vishal M. here!</h1></div>
+                    <div className='right_c'><img className="App-logo" alt="logo" src={self}></img></div>
+                </div>
             </div>
-        </div>
+        }
+        <CSSTransition
+            in={side_bar}
+            // nodeRef={nodeRef}
+            timeout={412}
+            classNames="App"
+            unmountOnExit
+            onEnter={() => toggleTopNav(false)}
+        // onExited={() => { toggleTopNav(true) }}
+        >
+            <div className={"App"}>
+                <Nav top_nav={top_nav} toggleTopNav={toggleTopNav} side_bar={side_bar} toggleSideBar={toggleSideBar} />
+                <div className='content'>
+                    <div className='left_c'><h1 className='typed'>Hi, Vishal M. here!</h1></div>
+                    <div className='right_c'><img className="App-logo" alt="logo" src={self}></img></div>
+                </div>
+            </div>
+        </CSSTransition>
+        <CSSTransition
+            in={top_nav}
+            // nodeRef={nodeRef}
+            timeout={412}
+            classNames="App_top"
+            unmountOnExit
+            onEnter={() => toggleSideBar(false)}
+            onExited={() => { toggleSideBar(true) }}
+        >
+            <div className={"App_top"}>
+                <Nav top_nav={top_nav} toggleTopNav={toggleTopNav} side_bar={side_bar} toggleSideBar={toggleSideBar} />
+                <div className='content'>
+                    <div className='left_c'><h1 className='typed'>Hi, Vishal M. here!</h1></div>
+                    <div className='right_c'><img className="App-logo" alt="logo" src={self}></img></div>
+                </div>
+            </div>
+        </CSSTransition>
+    </>
     )
 }
--- src/code/Nav.js
+++ src/code/Nav.js
@@ -1,37 +1,107 @@
+
 import React, { useState } from 'react'
 import './Nav.css'
 import { BsFillArrowUpRightSquareFill } from "react-icons/bs";
 import { HiBarsArrowUp, HiOutlineBars3CenterLeft, HiOutlineBarsArrowDown } from "react-icons/hi2";
 import { BiHide } from "react-icons/bi";
 import { FaReact } from "react-icons/fa";
+import { CSSTransition } from 'react-transition-group';
 
 
 export default function Nav(props) {
     let design_color = "#61dafb"
     let bgcolor = "#282c34"
     let font_color = "black"
-    return (<div className={props.nav_view == "top" ? 'nav_top' : "nav"}>
-        <div className={props.nav_view == 'left' ? 'nav_img_cont' : 'nav_img_cont_top'} >
-            {(!props.nav_view || props.nav_view == "top") && <HiOutlineBars3CenterLeft size={35} color={props.nav_view ? "black" : "#61dafb"}
-                onClick={() => { props.toogleNavView("left") }} />}
-            {(props.nav_view == "left") && <HiBarsArrowUp size={35}
-                onClick={() => { props.toogleNavView("top") }} />}
-            {props.nav_view && <FaReact className={props.nav_view == "left" ? 'hide' : "hide_top"} size={25} color={design_color}
-                onClick={() => { props.toogleNavView(false) }} />}
+    return (<div className={"nav"}>
+        <div className={'nav_img_cont'} >
+            {!props.side_bar && <HiOutlineBars3CenterLeft size={35} color={"#61dafb"}
+                onClick={() => { props.toggleSideBar(true); }}
+            />}
+
+            {props.side_bar && <HiBarsArrowUp size={35}
+                onClick={() => { props.toggleSideBar(false); setTimeout(() => { props.toggleTopNav(true) }, 450) }}
+            />}
+
+            {<FaReact className={'hide'} size={25} color={design_color}
+                onClick={() => { props.toggleTopNav(false); props.toggleSideBar(false) }}
+            />}
 
         </div>
 
-        {props.nav_view &&
-            <div className={props.nav_view == "left" ? 'nav_menus_left' : "nav_menus_top"}>
-                <div className='sign'>sign</div>
-                <div className={props.nav_view == "left" ? 'menus' : "left_menus"}>
+        {props.side_bar &&
+
+            <div className={'nav_menus_left'} style={{ background: design_color }}>
+                <div style={{ marginTop: 40 }} className='sign'>sign</div>
+                <div className={'menus'}>
                     <div className='nav1'>nav1</div>
                     <div className='nav1'>nav2</div>
                     <div className='nav1'>nav3</div>
                     <div className='nav1'>nav4</div>
                     <div className='nav1'>nav5</div>
                 </div>
-            </div>}
+            </div>
+        }
+
+        {props.top_nav &&
+            <div className={'nav_top'} style={{ background: design_color }}>
+                <div className='sign_top'>sign</div>
+                <div className={'nav_menus_top'}>
+                    <div className='nav1_top'>nav1</div>
+                    <div className='nav1_top'>nav2</div>
+                    <div className='nav1_top'>nav3</div>
+                    <div className='nav1_top'>nav4</div>
+                    <div className='nav1_top'>nav5</div>
+                </div>
+            </div>
+        }
+        {/* <CSSTransition
+            in={props.side_bar}
+            // nodeRef={nodeRef}
+            timeout={412}
+            classNames="nav_menus_left"
+            unmountOnExit
+            // onEnter={() => props.toggleTopNav(false)}
+            onExited={() => { props.toggleTopNav(true) }}
+        >
+
+
+            <div className={'nav_menus_left'}>
+                <div style={{ marginTop: 40 }} className='sign'>sign</div>
+                <div className={'menus'}>
+                    <div className='nav1'>nav1</div>
+                    <div className='nav1'>nav2</div>
+                    <div className='nav1'>nav3</div>
+                    <div className='nav1'>nav4</div>
+                    <div className='nav1'>nav5</div>
+                </div>
+            </div>
+
+        </CSSTransition>
+
+        <CSSTransition
+            in={props.top_nav}
+            // nodeRef={nodeRef}
+            timeout={412}
+            classNames="nav_top"
+            unmountOnExit
+            // onEnter={() => props.toggleTopNav("left")}
+            onExited={() => props.toggleSideBar(true)}
+        >
+
+
+            <div className={'nav_top'}>
+                <div style={{ marginTop: 40 }} className='sign'>sign</div>
+                <div className={'menus'}>
+                    <div className='nav1'>nav1</div>
+                    <div className='nav1'>nav2</div>
+                    <div className='nav1'>nav3</div>
+                    <div className='nav1'>nav4</div>
+                    <div className='nav1'>nav5</div>
+                </div>
+            </div>
+
+        </CSSTransition> */}
+
     </div>
     )
 }
